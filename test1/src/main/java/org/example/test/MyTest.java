ab374720c40449515846cc5bdc52dcbff4cdb64a
zhaojiang <zhaojiang@qq.com>
2022-09-13 14:49:54 +0800

    第二次提交程序

--- test1/src/main/java/org/example/test/MyTest.java
+++ test1/src/main/java/org/example/test/MyTest.java
@@ -3,5 +3,6 @@ package org.example.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hellogit2");
     }
 }
