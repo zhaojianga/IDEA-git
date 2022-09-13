72bb2450e54c1bc23d1eccd4ccc2fa4f36a7f8d5
zhaojiang <zhaojiang@qq.com>
2022-09-13 14:50:17 +0800

    第三次提交程序

--- test1/src/main/java/org/example/test/MyTest.java
+++ test1/src/main/java/org/example/test/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("hellogit2");
+        System.out.println("hellogit3");
     }
 }
